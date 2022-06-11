f93808b445ed8911f437079243e19c16e6bf2d29
rafaeloliva <roliva@uarg.unpa.edu.ar>
2022-06-11 15:29:56 -0300

    Moved ESPWRC2 from Private repo to Public - Example of ESP-Conv1 use with CL2 boards

---
+++ README.md
@@ -0,0 +1,14 @@
+## ESPWRC2 v2 - Private repo
+(Original version 2021 - updated 05.2022)
+
+### Introduction
+This repo contains ESPWRC2 - a new version of the ESPWRC2 web access for ESP-12 Module.
+Aimed to access PWRC2 systems used in INTI Test Site, using the CL2bm1 board [Ref1]. 
+See the /docs directory for more information. 
+
+### References
+
+[Ref1 - LyR AVR+Cypress](https://www.lyr-ing.com/Embedded/LyRAVR_CyEn.htm)
+
+
+
---
+++ include/version.h
@@ -0,0 +1,18 @@
+/*
+ ESPWRC2 R.Oliva 2022
+ 
+ Base on Web Server Enabled Sprinkler Controller
+ https://github.com/n0bel/ESPrinkler2
+ Original Base code is https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WebServer/examples/FSBrowser/FSBrowser.ino
+ Base Code Copyright (c) 2015 Hristo Gochkov. All rights reserved. LGPL
+ This Code Copyright (c) 2017 Kevin Uhlir. All rights reserved. LGPL
+ This is a rewrite of ESPrinkler https://github.com/n0bel/ESPrinkler but based
+ on the Arduino toolchain instead of ExperssIF.  This should make the project
+ more accessable to people.
+*/
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define VERSION "0.5.1"
+
+#endif  // VERSION_H_
